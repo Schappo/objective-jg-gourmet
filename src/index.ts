61996e135181301bfb271bd98286a7aec51843e4
Felipe Schappo <felipeschappoo@gmail.com>
2023-01-25 22:10:05 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+
+const addition = (a: number, b: number): number => {
+  return a + b
+}
+
+const number1 = 5
+const number2 = 12
+const result: number = addition(number1, number2)
+
+console.log('The result is %d', result)
